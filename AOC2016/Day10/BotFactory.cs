19438b552e228dcd9d0edb80518a3597abe1809e
Garrett Vernon <garrett.vernon@gmail.com>
2024-11-29 09:07:21 +0000

    Getting all tangled up!

--- AOC2016/Day10/BotFactory.cs
+++ AOC2016/Day10/BotFactory.cs
@@ -1,5 +1,10 @@
 namespace AOC2016.Day10;
 
+public enum ReceiverType
+{
+    Bot,
+    Output
+}
 /// <summary>
 /// Class <c>BotFactory</c> Makes and tracks bots in a factory.
 /// Also tracks the Output from bots
@@ -9,8 +14,36 @@ namespace AOC2016.Day10;
 public class BotFactory
 {
     private Dictionary<int, IReceiver> Receivers = new ();  // Track bots in factory 
-    private Dictionary<int, int> Outsputs = new Dictionary<int, int>(); // Track outputs
-    
+    private List<OutputBin> Outputs = new (); // Track outputs
+    private List<Bot> Bots = new();
+
+    public IReceiver GetReceiver(ReceiverType receiverType, int botId)
+    {
+        switch (receiverType)
+        {
+            case ReceiverType.Bot:
+                if (!Bots.Exists(b => b.Number == botId))
+                {
+                    Bots.Add(new Bot
+                    {
+                        Number = botId,
+                    });
+                }
+                return Bots.Find(b => b.Number == botId);
+            
+            case ReceiverType.Output:
+                if (!Outputs.Exists(b => b.Number == botId))
+                {
+                    Outputs.Add(new OutputBin
+                    {
+                        Number = botId,
+                    });
+                }
+                return Outputs.Find(b => b.Number == botId);
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
     /// <summary>
     /// This method returns a bot with the supplied <c>botid</c>
     /// </summary>
@@ -39,7 +72,8 @@ public class BotFactory
             string[] splitInstruction = instruction.Split(" ");
             if (splitInstruction[0] == "Bot")
             {
-                worker = (Bot)GetorAddBot(Int32.Parse(splitInstruction[1]));
+                worker = (Bot)GetReceiver(ReceiverType.Bot, 
+                    Int32.Parse(splitInstruction[1]));
                 // Check if there are instructions
                 if (splitInstruction.Length > 6)
                 {
--- AOC2016/Program.cs
+++ AOC2016/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        List<MyClass> myClasses = new List<MyClass>
+        {
+            new MyClass { Number = 3 },
+            new MyClass { Number = 4 },
+            new MyClass { Number = 5 },
+        };
+        Console.WriteLine(myClasses.Find(c => c.Number == 6));
     }
+}
+
+class MyClass : IEquatable<MyClass>
+{
+    public int Number { get; set; }
+    public bool Equals(MyClass? other)
+    {
+        if (other == null) return false;
+        return (this.Number.Equals(other.Number));
+    }
+
+    // public override string ToString()
+    // {
+    //     return $"MyClass {Number}";
+    // }
 }
