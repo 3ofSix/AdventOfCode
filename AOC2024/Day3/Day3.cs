5fc12aceb079c7f28618f44d781a0de590e0a095
Vernon, Garrett <garrett.vernon@daera-ni.gov.uk>
2024-12-03 10:40:16 +0000

    Day3 part1 complete

---
+++ AOC2024/Day3/Day3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using System.Text.RegularExpressions;
+
+namespace AOC2024.Day3
+{
+    public class Day3
+    {
+        private const string PATTERN = @"mul\((\d{1,3}),(\d{1,3})\)";
+
+        public void Part1(string filePath)
+        {
+            List<int> instructions = new();
+            try
+            {
+                using StreamReader reader = new StreamReader(filePath);
+                
+                string input = reader.ReadToEnd();
+
+                foreach (Match match in Regex.Matches(input,PATTERN))
+                {
+                    Console.WriteLine($"Found {match.Value} at {match.Index}: Groups: {match.Groups[1]}, {match.Groups[2]}");
+                    instructions.Add(int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
+                }
+
+                Console.WriteLine($"Sum: {instructions.Sum()}");
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public void Part2()
+        {
+
+        }
+    }
+}
--- AOC2024/Program.cs
+++ AOC2024/Program.cs
@@ -2,14 +2,20 @@
 
 using AOC2024.Day1;
 using AOC2024.Day2;
+using AOC2024.Day3;
 
 
 //string p = @"C:\Users\admin\RiderProjects\AdventOfCode\AOC2024\Day1\sample.txt";
-string p = @"../../../Day1/puzzle.txt";
-Day1 day1 = new Day1(p);
-day1.Part1();
-day1.Part2();
+//string p = @"../../../Day1/puzzle.txt";
+//Day1 day1 = new Day1(p);
+//day1.Part1();
+//day1.Part2();
 
 //string p = @"../../../Day2/sample.txt";
 // string p = @"../../../Day2/puzzle.txt";
 // Day2 day2 = new Day2(p);
+
+//string p = @"../../../Day3/sample.txt";
+string p = @"../../../Day3/puzzle.txt";
+Day3 day3 = new Day3();
+day3.Part1(p);
