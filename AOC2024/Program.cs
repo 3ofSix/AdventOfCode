57f486c1c36701e6efe1921e01dd22818f1cb59d
Garrett Vernon <garrett.vernon@gmail.com>
2024-12-04 19:49:35 +0000

    Added menu for running solver

--- AOC2024/Program.cs
+++ AOC2024/Program.cs
@@ -6,29 +6,70 @@ using AOC2024.Day3;
 using Version = AOC2024.Version;
 using AOC2024.Day4;
 
-int day = 2;
-var v = Version.Sample;
+uint day;
+Version version = new Version();
+bool isExit = false;
 
-        
-string file = v == Version.Sample ? $@"../../../Day{day}/sample.txt" : $@"../../../Day{day}/puzzle.txt";
-switch (day)
+void Solve()
 {
-    case 1:
-        Day1 day1 = new Day1(file);
-        day1.Part1();
-        day1.Part2();
-        break;
-    case 2: 
-        Day2 day2 = new Day2(file);
-        break;
-    case 3:
-        Day3 day3 = new Day3(file);
-        day3.Part1();
-        day3.Part2();
-        break;
-        
-        
-        
-    default:
-        throw new Exception($"Day {day} is not supported");
+    string file = version == Version.Sample ? $@"../../../Day{day}/sample.txt" : $@"../../../Day{day}/puzzle.txt";
+    switch (day)
+    {
+        case 1:
+            Day1 day1 = new Day1(file);
+            day1.Part1();
+            day1.Part2();
+            break;
+        case 2:
+            Day2 day2 = new Day2(file);
+            break;
+        case 3:
+            Day3 day3 = new Day3(file);
+            day3.Part1();
+            day3.Part2();
+            break;
+        case 4:
+            Day4 day4 = new Day4(file);
+            day4.Part1("XMAS");
+            break;
+
+        default:
+            throw new Exception($"Day {day} is not supported");
+    }
+}
+
+while (!isExit)
+{
+    Console.WriteLine("\n\tAdvent of Code 2024");
+    Console.WriteLine("\nWhat day do you want to solve?\nType a number for the day?\n\t0 for exit");
+    day = Convert.ToUInt32(Console.ReadLine());
+    if (day == 0) break;
+    Console.WriteLine("----------------");
+    Console.WriteLine("Attempt solve for sample or puzzle input?");
+    Console.WriteLine("\t1 - Sample \n\t2 - Puzzle");
+
+    Console.WriteLine("\t0 - Exit");
+
+    switch (Convert.ToUInt32(Console.ReadLine()))
+    {
+        case 1:
+            version = Version.Sample;
+            break;
+        case 2:
+            version = Version.Puzzle;
+            break;
+        default:
+            isExit = true;
+            break;
+    }
+    if (isExit) break;
+    try
+    {
+        Solve();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+
 }
