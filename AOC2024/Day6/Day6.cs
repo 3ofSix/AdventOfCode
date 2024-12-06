3bb089102cdde9f7600fcf4bb1a721b9f036f263
Garrett Vernon <garrett.vernon@gmail.com>
2024-12-06 20:37:40 +0000

    Version 1. Sample works. Puzzle answer 4441 too low!

--- AOC2024/Day6/Day6.cs
+++ AOC2024/Day6/Day6.cs
@@ -1,4 +1,6 @@
-﻿namespace AOC2024.Day6
+﻿using System.Drawing;
+
+namespace AOC2024.Day6
 {
     public class Day6
     {
@@ -17,7 +19,9 @@
             // Find Guard start coords
             // Find start direction
             Guard guard =  FindGuard();
+            Console.WriteLine($"Guard starts at ({guard.X},{guard.Y}): moving {guard.Direction} ");
            // Move Guard and track path
+           MoveGuard(guard);
 
         }
 
@@ -39,9 +43,9 @@
                         {
                             return new Guard
                             {
-                                x = x,
-                                y = y,
-                                direction = direction
+                                X = x,
+                                Y = y,
+                                Direction = direction
                             };
                         }
                     }
@@ -50,19 +54,92 @@
             return null;
         }
 
-        private void MoveGuard()
+        private void MoveGuard(Guard guard)
         {
+            List<string> points = new();
+            // Add Guard start position
+            points.Add(string.Concat(guard.X, guard.Y)); 
+            // While the guard is in the room move
+            // ^ : row -1, > : col +1, v : row +1, < : col -1
+            
+            // CHeck boundaries
+            bool guardInRoom = true;
+            // while (guard.X < _room[0].Length && guard is { X: > 0, Y: > 0 } && guard.Y < _room.Length)
+            while (guardInRoom)
+            {
+                // Check ahead, if cannot (#) move turn right, check ahead again
+                // move in a direction, record position, then check ahead
+                // repeat
 
+                // record position here
+                
+                // Console.WriteLine($"Guard at ({guard.X},{guard.Y}): moving {guard.Direction} ");
+                points.Add(string.Concat(guard.X, guard.Y));
+                try
+                {
+                    switch (guard.Direction)
+                                    {
+                                        case '^':
+                                            if (_room[guard.X - 1][guard.Y] != '#')
+                                            {
+                                                guard.X -= 1;
+                                            }
+                                            else
+                                            {
+                                                guard.Direction = '>';
+                                            }
+                                            break;
+                                        case 'v':
+                                            if (_room[guard.X + 1][guard.Y] != '#')
+                                            {
+                                                guard.X += 1;
+                                            }
+                                            else
+                                            {
+                                                guard.Direction = '<';
+                                            }
+                                            break;
+                                        case '<':
+                                            if (_room[guard.X][guard.Y - 1] != '#')
+                                            {
+                                                guard.Y -= 1;
+                                            }
+                                            else
+                                            {
+                                                guard.Direction = '^';
+                                            }
+                                            break;
+                                        case '>':
+                                            if (_room[guard.X][guard.Y + 1] != '#')
+                                            {
+                                                guard.Y += 1;
+                                            }
+                                            else
+                                            {
+                                                guard.Direction = 'v';
+                                            }
+                                            break;
+                                        default:
+                                            throw new Exception("Invalid direction");
+                                    }
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"\n\tGuard has left the building!");
+                    Console.WriteLine($"\tGuard was in {points.Distinct().Count()} positions.");
+                    Console.ResetColor();
+                    guardInRoom = false;
+                }
+            }
         }
-
-        
     }
 
     public class Guard
     {
-        public int x { get; init; }
-        public int y { get; init; }
-        public char direction { get; init; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Direction { get; set; }
 
 
     }
